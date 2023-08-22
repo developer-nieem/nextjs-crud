bfc6c9991e4fb13b5690b27c964bc8e585cefd8d
developer-nieem <naimhossenwork@gmail.com>
2023-08-22 16:33:03 +0600

    complete add topics part

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+'use client'
+import React, {  useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 
+const AddTopic = () => {
 
-const AddTopic = async() => {
+    const [title, setTitle] =  useState('');
+    const [description, setDescription] =  useState('');
+    const router =  useRouter()
+    const submitHandler = async(e) => {
+        e.preventDefault();
 
+        try {
+       const res =   await  fetch('http://localhost:3000/api/topics', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            
+            body:JSON.stringify({title, description})
+          })
+
+          if (res.ok) {
+            router.push('/')
+          }
+        } catch (error) {
+            
+        }
+    }
    
     return (
         <div>
-            <form className='flex flex-col my-5 space-y-3'>
-                <input className='p-2 border border-slate-600 ' type="text" placeholder='Title' name='title' />
-                <textarea cols='6' rows='6' className='p-2 border border-slate-600 ' type="text" placeholder='Discription' name='title' />
+            <form onSubmit={submitHandler} className='flex flex-col my-5 space-y-3'>
+                <input onChange={(e)=> setTitle(e.target.value)} className='p-2 border border-slate-600 ' type="text" placeholder='Title' name='title' required />
+                <textarea onChange={(e)=> setDescription(e.target.value)} cols='6' rows='6' className='p-2 border border-slate-600 ' type="text" placeholder='Discription' name='title'  required/>
 
                 <button className='bg-stone-600 text-white  p-2 w-fit'>Added Topic</button>
             </form>
